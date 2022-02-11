3c7ec26f4594259491376f507644c2f00f66c31d
I-Indorf <irene.indorf@hotmail.com>
2022-02-10 19:57:39 -0600

    added JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const createButton = document.getElementById("create-button");
+createButton.onclick = onCreateButtonClicked;
+function onCreateButtonClicked() {
+    console.log(descriptionBox.value);
+    console.log(priorityBox.value);
+    console.log(dueDateBox.value);
+    const desc = descriptionBox.value;
+    const priority = priorityBox.value;
+    const date = dueDateBox.value;
+    
+    if(desc === "") {
+        return;
+    }
+    console.log("I got here");
+}
+
+const descriptionBox = document.getElementById("description");
+const priorityBox = document.getElementById("priority");
+const dueDateBox = document.getElementById("due-date");
+
