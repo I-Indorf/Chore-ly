dc822e409d96922de9d2ab2fee8ea0b17679cac4
I-Indorf <irene.indorf@hotmail.com>
2022-02-13 12:52:21 -0600

    Added more JS

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const createButton = document.getElementById("create-button");
 createButton.onclick = onCreateButtonClicked;
 function onCreateButtonClicked() {
-    console.log(descriptionBox.value);
-    console.log(priorityBox.value);
-    console.log(dueDateBox.value);
     const desc = descriptionBox.value;
     const priority = priorityBox.value;
     const date = dueDateBox.value;
     
-    if(desc === "") {
+    if(desc === "" || priority === "" || date === "") {
         return;
     }
-    console.log("I got here");
+    let newRow = document.createElement("tr");
+    newRow.innerHTML = "New List Item";
+
+    let list = document.querySelector('table#to-do-table tbody');
+    list.appendChild(newRow); 
 }
 
 const descriptionBox = document.getElementById("description");
 const priorityBox = document.getElementById("priority");
 const dueDateBox = document.getElementById("due-date");
 
+{/* <tr>
+            <td><input type="checkbox" id="item1"></td>  
+          <td>High</td>
+          <td>Make the App</td>
+          <td>2-15-22</td>
+          <td><button type="button">Remove</button>
+          </td> 
+        </tr> */}
